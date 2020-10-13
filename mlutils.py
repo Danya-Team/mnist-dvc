425632c08bf36fec5656aa56fe1b98782bac234f
davi.he <davi.he@sunvalley.com.cn>
2020-10-13 16:43:03 +0800

    update logs and models

--- mlutils.py
+++ mlutils.py
@@ -1,7 +1,21 @@
+
+import numpy as np
+import matplotlib.image as mpimg
+from matplotlib import pyplot as plt
+from tensorflow.keras.utils import plot_model
+import tensorflow as tf
+from keras import layers
+import keras
+import platform
 import subprocess
 import os
 from datetime import datetime
 
+import logging
+import coloredlogs
+
+coloredlogs.install(level='INFO')
+log = logging.getLogger('mlutils')
 
 def timestamp():
     return datetime.now().strftime("%Y-%d-%m_%H-%M-%S-%f-")
@@ -33,3 +47,19 @@ def git_version():
         GIT_REVISION = "Unknown"
 
     return GIT_REVISION
+
+
+def mlversion():
+    from IPython.core.interactiveshell import InteractiveShell
+
+    InteractiveShell.ast_node_interactivity = "all"
+    from keras import models
+
+    strategy = tf.distribute.MirroredStrategy()
+    log.info("python version       :" + platform.python_version())
+    log.info("keras version        :" + keras.__version__)
+    log.info("tensorflow version   :" + tf.__version__)
+    log.info("Number of GPU devices: {}".format(strategy.num_replicas_in_sync))
+
+    log.info(tf.config.list_physical_devices('GPU'))
+    log.info(tf.sysconfig.get_lib())
--- mnist-keras.py
+++ mnist-keras.py
@@ -1,4 +1,4 @@
-from mlutils import git_version, timestamp
+from mlutils import git_version, mlversion, timestamp
 import git
 from datetime import datetime
 from keras.utils import to_categorical
@@ -9,22 +9,28 @@ from matplotlib import pyplot as plt
 import tensorflow as tf
 import keras
 import platform
-from IPython.core.interactiveshell import InteractiveShell
 import sys
 import subprocess
 import os
-InteractiveShell.ast_node_interactivity = "all"
+from tensorflow.python.keras.utils.vis_utils import plot_model
+import matplotlib.image as mpimg
+import logging
+import coloredlogs
+
+coloredlogs.install(level='DEBUG')
+log = logging.getLogger('mnist-keras')
 
 MODEL_VERSION = timestamp() + git_version() + "-V1.0"
 
-print("python version       :" + platform.python_version())
-print("keras version        :" + keras.__version__)
-print("tensorflow version   :" + tf.__version__)
-print(MODEL_VERSION)
+log.warning(MODEL_VERSION)
+log.error(MODEL_VERSION)
+
+mlversion()
 
 # sys.exit()
 
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data(
+    os.getcwd() + "/datasets/mnist.npz")
 
 train_images.shape
 train_labels.shape
@@ -35,12 +41,18 @@ img = train_images[100]
 plt.imshow(img, cmap=plt.cm.binary)
 
 
-model = keras.Sequential(name=MODEL_VERSION)
-model.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
-model.add(layers.Dense(10, activation='softmax'))
+def build_mode():
+    model = keras.Sequential(name=MODEL_VERSION)
+    model.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+    model.add(layers.Dense(10, activation='softmax'))
 
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+    plot_model(model, to_file='models1.png',
+               show_shapes=True, show_layer_names=True)
+    # plt.imshow(mpimg.imread("models1.png"))
+    return model
+
 
 train_images = train_images.reshape(60000, 28*28)
 train_images = train_images.astype('float32')/255
@@ -49,6 +61,10 @@ train_labels = to_categorical(train_labels)
 test_images = test_images.reshape(10000, 28*28)
 test_images = test_images.astype('float32')/255
 test_labels = to_categorical(test_labels)
+
+
+with tf.distribute.MirroredStrategy().scope():
+    model = build_mode()
 
 model.fit(train_images, train_labels, epochs=2, batch_size=128)
 
